4a7ec941e4f747da613630875faaf22916d55450
corfaction <corfaction@gmail.com>
2026-08-17 16:47:00 +0500

    chore: initialize Ancient Artifacts

---
+++ src/client/java/net/corfaction/ancientartifacts/client/AncientArtifactsClient.java
@@ -0,0 +1,10 @@
+package net.corfaction.ancientartifacts.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class AncientArtifactsClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
